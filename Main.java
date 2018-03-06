b384ecc2b3694f5038e56ba24d48206aa6883221
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-06 21:02:25 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            About.numberOfTask();
+
+
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
+
+
