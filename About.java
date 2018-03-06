b3f910d9bb16122db52c1e326f192498319ca0cb
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-06 21:02:52 +0300

    Create About.java

---
+++ About.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by Student on 06.03.2018.
+ */
+public class About {
+    public static void numberOfTask() throws Exception
+
+    {
+        System.out.println("Выберите номер задачи (1~8): ");
+
+        int choice = -1;
+
+        Scanner in = new Scanner(System.in);
+        try {
+            choice = Integer.parseInt(in.next());
+        }catch (Exception e){
+            throw new Exception("Ошибка ввода!");
+        }
+
+        switch (choice) {
+            case 1:
+                Task1.tab();
+                break;
+            case 2:
+                ;
+                break;
+            case 3:
+                ;
+                break;
+            case 4:
+                ;
+                break;
+            case 5:
+                ;
+                break;
+            case 6:
+                ;
+                break;
+            case 7:
+                ;
+                break;
+            case 8:
+                ;
+                break;
+            default:
+                throw new Exception("Задачи с таким номером нет!");
+        }
+
+
+    }
+
+}
