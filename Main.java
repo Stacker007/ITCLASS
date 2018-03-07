9ce0f30d251fc4e2f3ea0374acce81d18a7f2568
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-07 20:36:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,14 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             About.numberOfTask();
-
-
-
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
 
     }
 }
-
-
