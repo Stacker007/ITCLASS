0db0f49eb8b6d0ce94f251ac82cf2b8406653dd0
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-07 20:51:59 +0300

    Create Task7.java

---
+++ Task7.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by Student on 07.03.2018.
+ *
+ * Задана строка, содержащая произвольный текст. Найти и подсчитать количество вхождений второй строки в первую.
+ */
+public class Task7 {
+    public static void tab(){
+        String all, part;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите произвольный текст: ");
+        all=in.nextLine();
+        System.out.println("Введите искомую часть: ");
+        part=in.nextLine();
+        boolean flag =true;
+        while (flag){
+            flag=false;
+            all.
+        }
+
+    }
+
+}
