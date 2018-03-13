c6538e20876fa61ee11d463c80f64317d9ed86a8
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-13 16:53:35 +0300

    Create Task8.java

---
+++ Task8.java
@@ -0,0 +1,54 @@
+package com.company;
+
+/*Заданы поля структуры Автомобиль:
+Марка,
+Пробег,
+Стоимость.
+Создать массив элементов структуры.
+Ввести и вывести соответствующие данные, а
+также вывести все записи, стоимость которых ниже указанной.
+
+ */
+
+import java.util.Scanner;
+
+public class Task8 {
+
+
+    public static void tab() throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество автомобилей: ");
+        int countOfCars;
+        try {
+            countOfCars= Integer.parseInt(in.nextLine());
+
+        }catch (Exception e) {
+            throw new Exception("Ошибка ввода!");
+        }
+        Vehicles[] your = new Vehicles[countOfCars];
+        for (int i=0;i<countOfCars;i++) {
+            your[i] = new Vehicles();
+        }
+        your[0]=new Vehicles();
+        your[1]=new Vehicles();
+
+
+        System.out.println("Введите максимальную стоимость автомобиля: ");
+        int costGeneral;
+        try {
+            costGeneral = Integer.parseInt(in.nextLine());
+
+        }catch (Exception e) {
+            throw new Exception("Ошибка ввода!");
+        }
+
+        System.out.println("Выбранные автомобили:");
+
+        for (int i=0;i<countOfCars;i++){
+            if(your[i].retCost()<costGeneral)your[i].printParametres();
+
+        }
+
+
+    }
+}
