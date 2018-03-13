e1deb0bdc90b96b9244c4ce46d9a82c742397831
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-03-13 15:34:01 +0300

    Update Task7.java

--- Task7.java
+++ Task7.java
@@ -15,11 +15,26 @@ public class Task7 {
         all=in.nextLine();
         System.out.println("Введите искомую часть: ");
         part=in.nextLine();
-        boolean flag =true;
-        while (flag){
-            flag=false;
-            all.
+
+        int i = 0;
+        int count = 0;
+        while (i<all.length()){
+            if (all.charAt(i)==part.charAt(0)){
+                int j=0;
+                boolean flag = true;
+                while (j<part.length()){
+                    //Если находится отличный символ или произвольная строка заканчивается
+                    if (part.charAt(j)!=all.charAt(i+j)||(i+j+1)==all.length()) {
+                        flag = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (flag==true) count++;
+            }
+            i++;
         }
+        System.out.println("Ответ: " + count);
 
     }
 
